43a748537f501f8d1c623f551934db78cdae6ef6
Bill Wang <4349722@qq.com>
2017-12-23 16:09:31 +0800

    String test.

---
+++ fancy_credits.py
@@ -0,0 +1,16 @@
+# Fancy Credits
+
+print("\t\t\tFancy Credits")
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\tby")
+print("\t\t\tBill Wang")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+print("\nSpecial thanks goes out to:")
+print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
+
+# Beep
+print("\a")
+
+input("\n\nPress the enter key to exit.")
---
+++ game_over2.py
@@ -0,0 +1,27 @@
+# Game Over - Version 2
+
+print("Program 'Game Over' 2.0")
+
+print("Same", "message", "as before")
+
+print("Just",
+      "a bit",
+      "bigger")
+
+print("Here", end=" ")
+print("it is ...")
+
+print(
+	"""
+	 _____
+        /  __|  
+        | |
+        | |
+        | |  _
+        | |_| |
+        \____/
+	"""
+	)
+
+input("\n\nPress the enter key to exit.")
+
